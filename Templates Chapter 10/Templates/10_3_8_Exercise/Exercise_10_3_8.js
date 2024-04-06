f4ec30084f8c3cb5f1c9b82cae74f4d1890a054f
abolo <abolfazle0915501@example.com>
2024-04-06 15:58:57 +0200

    efr

--- Templates Chapter 10/Templates/10_3_8_Exercise/Exercise_10_3_8.js
+++ Templates Chapter 10/Templates/10_3_8_Exercise/Exercise_10_3_8.js
@@ -3,6 +3,10 @@ var countries=["Luxembourg","USA","France","Germany","Italy"];
 function fillInSelect()
 {
     //code here
+   var selectionList = document.getElementById("listOfCountries")
+    for(let i=0; i<countries.length; i++){
+        ??? = "<Option>"+countries[i]+"<Option/>"
+    }
 }
 
 function selectThisCountry()
